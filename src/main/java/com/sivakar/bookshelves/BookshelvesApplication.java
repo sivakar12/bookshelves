633ddc0441fd7f96f31694868843be327114db35
Sivakar Sithamparanathan <sivakar12@outlook.com>
2024-02-21 22:20:12 +0530

    Create project using Spring Initializr

---
+++ src/main/java/com/sivakar/bookshelves/BookshelvesApplication.java
@@ -0,0 +1,13 @@
+package com.sivakar.bookshelves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivakar/bookshelves/BookshelvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivakar.bookshelves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
